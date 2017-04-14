751cba29a38507eb6e0b116eed2362670c8cb820
BRANDON\Brandon <brandon.bn.nelson@gmail.com>
2017-04-14 12:40:01 -0700

    Has fullscreen picture closing and choosing different thumbnails for fullscreen

--- nelsonsUpholstery/src/components/Gallery/Gallery.js
+++ nelsonsUpholstery/src/components/Gallery/Gallery.js
@@ -5,15 +5,43 @@ import Picture from '../Picture/Picture';
 const galleryData = require('../../gallery.json');
 
 class Gallery extends Component {
-
+// store entire obj that is selected? all images?
+// onClick passed down and then is triggered (so set the pictures selected above) 
     constructor (props) {
         super(props);
-        
+         this.state = {
+            isExpandedPopUp: false,
+            selectedThumbnailItem: galleryData.features[0],
+            selectedThumbnailImage: galleryData.features[0].pictures[0]
+        };
+
+        this.expandImage = this.expandImage.bind(this);
+        this.setSelectedThumbnail = this.setSelectedThumbnail.bind(this);
         this.renderItemArea = this.renderItemArea.bind(this);
+        this.renderFullScreenPopup = this.renderFullScreenPopup.bind(this);
+        this.closeFullScreenViewer = this.closeFullScreenViewer.bind(this);
+    }
+
+    expandImage() {
+        this.setState({isExpandedPopUp: true});
+    }
+
+    setSelectedThumbnail(i) {
+        this.expandImage();
+        this.setState({
+            selectedThumbnailItem: i,
+            selectedThumbnailImage: i.pictures[0]
+        });
+    }
+
+    closeFullScreenViewer() {
+        this.setState({
+            isExpandedPopUp: false,
+            selectedThumbnailImage: null
+        });
     }
 
     renderItemArea (item, key) {
-        let images = item.pictures.map((item, i) => <Picture item={item} key={i}/>);
         return (
             <div key={key} className='-pictureArea'>
                 <div className='-top'>
@@ -25,17 +53,45 @@ class Gallery extends Component {
                     </div>
                 </div>
                 <div className='-bottom'>
-                    {images}
+                    <Picture featureItem={item} setItem={this.setSelectedThumbnail}/>
                 </div>
             </div>
         );
     }
 
+    renderFullScreenPopup () {
+        let thumbnail = this.state.selectedThumbnailItem.pictures.length > 1 ?
+        <div className='-thumbnailContainer'>
+        {
+            this.state.selectedThumbnailItem.pictures.map((item, i) => {
+                return (
+                    <div key={i} className='-thumbnailPictureContainer'>
+                        <img src={item.src} className='-thumbnailImage' alt={i} width='100%' height='100%' onClick={() => this.setState({selectedThumbnailImage: item})}/>
+                    </div>
+                );
+            })
+        }
+        </div>
+        : 
+        null;
+        return (
+            <div className='-fullScreenContainer'>
+                <div className='-fullScreenPic'>
+                    <img src={this.state.selectedThumbnailImage.src} className='-fullScreenThumbnail' alt={this.state.selectedThumbnailItem.title}/>
+                    <img src='../../../closeFullImage.svg' className='-closeFullScreenPic' onClick={this.closeFullScreenViewer}/>
+                </div>
+                
+                {thumbnail}
+            </div>
+        );
+    }
+
     render() {
-        console.log(galleryData);
+
         return (
             <div className='-gallery'>
                 {galleryData.features.map((item, key) => this.renderItemArea(item, key))}
+                {this.state.isExpandedPopUp ? this.renderFullScreenPopup() : null}
             </div>
         );
     }
--- nelsonsUpholstery/src/components/Picture/Picture.js
+++ nelsonsUpholstery/src/components/Picture/Picture.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react';
 import './Picture.css';
 
-class Gallery extends Component {
-
-    constructor (props) {
-        super(props);
-
-        this.state = {isExpandedPopUp: false}
-
-        this.expandImage = this.expandImage.bind(this);
-    }
-
-    expandImage() {
-        this.setState({isExpandedPopUp: !this.state.isExpandedPopUp});
-    }
-
+class Picture extends Component {
     render() {
-        let fullScreenImg = (
-            <div className='-fullScreenContainer'>
-                <div className='-fullScreenPic'>
-                    <img src={this.props.item.src} alt={this.props.item.title}/>
-                </div>
-            </div>
-        );
         return (
-            <div className='-picture' onClick={this.expandImage}>
-                <img src={this.props.item.src} alt={this.props.item.title}/>
-                {this.state.isExpandedPopUp ? fullScreenImg : null}
+            <div className='-picture' onClick={() => this.props.setItem(this.props.featureItem)}>
+                <img src={this.props.featureItem.pictures[0].src} className='-pictureThumbnailImage' alt={this.props.featureItem.pictures[0].title}/>
+                
             </div>
         );
     }
 }
 
-export default Gallery;
+export default Picture;
 
 
 
